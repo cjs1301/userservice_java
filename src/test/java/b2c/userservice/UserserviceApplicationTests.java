864b952e04eb85d5868538bdfbd3f1f5004329e1
jaesong <cjs5241@gmail.com>
2022-06-05 23:33:12 +0900

    init

---
+++ src/test/java/b2c/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package b2c.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
